163ac18636aa9f2a9d4058f8765831085d82858f
Guy Roger Eboulet <eboulet87@gmail.com>
2019-12-17 16:45:18 +0100

    Commit finale

--- js/mioscript.js
+++ js/mioscript.js
@@ -1,7 +1,9 @@
 alert("ciao");
 var nome = prompt("Il tuo nome ?");
 console.log(nome);
-var cognome = prompt("il tuo cognome ?");
+var cognome = prompt("Il tuo colore cognome ?");
 console.log(cognome);
 var colorePreferito = prompt("Il tuo colore preferito ?");
 console.log(colorePreferito);
+var numero = "19"
+document.getElementById('password').innerHTML = nome + cognome + colorePreferito + numero;
